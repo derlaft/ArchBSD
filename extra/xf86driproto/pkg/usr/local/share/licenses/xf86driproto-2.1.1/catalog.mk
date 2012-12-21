dcda844d011e8fd121db0a744bcc037739871c22
Charlie Root <root@Bahamut.home>
2012-12-21 01:14:31 +0000

    xf86driproto PKGBUILD:   xf86driproto/PKGBUILD

---
+++ extra/xf86driproto/pkg/usr/local/share/licenses/xf86driproto-2.1.1/catalog.mk
@@ -0,0 +1,5 @@
+_LICENSE=MIT
+_LICENSE_NAME=MIT license / X11 license
+_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
+_LICENSE_GROUPS=FSF GPL OSI COPYFREE
+_LICENSE_DISTFILES=xf86driproto-2.1.1.tar.bz2
